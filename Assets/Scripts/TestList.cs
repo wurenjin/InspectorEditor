4f9ea48bda98e67c92427db7cb2265926025a7cb
wurenjin <53325728+wurenjin@users.noreply.github.com>
2021-09-23 16:46:04 +0800

    reorderableList
    reorderableList

---
+++ Assets/Scripts/TestList.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestList : MonoBehaviour 
+{
+    public List<Color> m_colors = new List<Color>();
+
+    private void Start()
+    {
+
+    }
+}
